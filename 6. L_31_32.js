39ccc8eddff1e0489e6d94fa74b6d1f890f4a234
Sara <59883015+Sara1620@users.noreply.github.com>
2024-03-07 02:40:36 +0300

    Add files via upload

---
+++ 6. L_31_32.js
@@ -0,0 +1,36 @@
+console.log("****************Assignment1********************");
+
+console.log(100 == "100"); // true
+console.log(100 < 1000); // true
+console.log(110 > 100 > 10 < 20); // true
+console.log(-10 == "-10"); // true
+console.log(!(-50 >= +"-40")); // true
+console.log(typeof 10 == typeof -'-40'); // true
+console.log(!('10' === 10)); // true
+console.log(!(20 < false)); // true
+
+
+console.log("****************Assignment2********************");
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1<num2); // true
+console.log(num1<=num2); // true
+console.log(num1!=num2); // true
+console.log(num1!==num2); // true
+console.log(!(num1===num2)); // true
+console.log(typeof num1 == typeof num2); // true 
+console.log(num1&&true); // true
+console.log(true||num2); // true
+
+
+/******************************Assignment3**********************************/
+
+let a = 20;
+let b = 30;
+let c = 10;
+
+console.log("1.",a > b && a < c || a < b); // true
+console.log("2.",a < b && a > c); // true
+console.log("3.",!(a > b) && !(a == b) && !(a < c) && !(a === c)); // true
